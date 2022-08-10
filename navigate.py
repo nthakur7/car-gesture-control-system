e74d9414d5826c0d3a935a5194006919e2e297bc
Nandini <nandini.s.thakur@gmail.com>
2022-08-09 19:31:17 -0700

    Update navigate.py

--- navigate.py
+++ navigate.py
@@ -13,11 +13,6 @@ def request_handler(request):
                 startlon = float(request['form']['startlon'])
                 startlat = float(request['form']['startlat'])
 
-                # startlon = -71.090255
-                # startlat = 42.36164
-
-
-                #txt = 'https://api.mapbox.com/directions/v5/mapbox/driving/-71.090255%2C42.36164%3B-71.0761796953%2C42.3416286335?alternatives=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token=pk.eyJ1Ijoic2FtYXlnb2Rpa2EiLCJhIjoiY2wyYzBwNHl5MDF6ZzNqbzNmM3p3ZGJxNCJ9.NtOXKbVg7nxc9DFMYU5amw'
                 txt = 'https://api.mapbox.com/directions/v5/mapbox/driving/{}%2C{}%3B{}%2C{}?alternatives=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token=pk.eyJ1Ijoic2FtYXlnb2Rpa2EiLCJhIjoiY2wyYzBwNHl5MDF6ZzNqbzNmM3p3ZGJxNCJ9.NtOXKbVg7nxc9DFMYU5amw'.format(startlon, startlat, dest_lon, dest_lat)
                 
                 r = requests.get(txt)
@@ -31,7 +26,7 @@ def request_handler(request):
                             outs += step["maneuver"]["instruction"]
                             outs += '\n'
 
-                return outs #response["routes"][0]["legs"][0]["steps"][0]
+                return outs
 
                 for route in response["routes"]:
                     inner = route["legs"]
